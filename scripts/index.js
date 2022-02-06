4790f8a60e8e968bfbd14702d3565dbfc22272db
Roman Kamelianov <r.kamelyanov@yandex.ru>
2022-02-06 23:34:05 +0300

    refactor html css js

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,31 @@
 let editFormButtonOpen = document.querySelector('.profile__name-edit')
 let editForm = document.querySelector('.edit-form')
-let editFormButtonGlose = editForm.querySelector('.edit-form_glose')
+let editFormButtonGlose = editForm.querySelector('.edit-form__glose')
 
 let editFormOpen = function () {
+  editForm.classList.remove('edit-form');
   editForm.classList.add('edit-form_open');
+  console.log('jnrhskb');
 };
 
 let editFormGlose = function () {
   editForm.classList.remove('edit-form_open');
 };
 
-editFormButtonOpen.addEventListener('click', editFormOpen);
+editFormButtonOpen.addEventListener('click', editFormOpen); 
 editFormButtonGlose.addEventListener('click', editFormGlose);
 
 let addProfileName = document.querySelector('.profile__info-name')
 
-
-console.log(addProfileName)
-
 function addName() {
-  let InputProfileName = document.querySelector(`.input__text_name`);
-  let InputProfileNameDescription = document.querySelector(`.input__text_description`);
+  let InputProfileName = document.querySelector(`.input__text-name`);
+  let InputProfileNameDescription = document.querySelector(`.input__text-description`);
 
   addProfileName.innerHTML = `
   <h1 class="profile__name-title">${InputProfileName.value}</h1>
   <p class="profile__info-description">${InputProfileNameDescription.value}</p>
   `
 }
-let SaveFormName = document.querySelector(`.form__submit-btn_save`)
+
+let SaveFormName = document.querySelector(`.form__submit-btn-save`)
 SaveFormName.addEventListener('click', addName) 
