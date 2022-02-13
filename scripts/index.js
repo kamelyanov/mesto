fd49ea84075a0badeb09ef8d6ca39e0f22ef8c17
Roman Kamelianov <r.kamelyanov@yandex.ru>
2022-02-13 12:24:21 +0300

    2 review

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,22 @@ let editFormButtonOpen = document.querySelector('.profile__name-edit')
 let editForm = document.querySelector('.edit-form')
 let editFormButtonGlose = editForm.querySelector('.edit-form__glose')
 let pagecover = document.querySelector('.page__cover')
-let inputProfileName = document.querySelector('.edit-form__input-name');
-let inputProfileNameDescription = document.querySelector('.edit-form__input-description');
+let inputProfileName = document.querySelector('.edit-form__input_type_name');
+let inputProfileNameDescription = document.querySelector('.edit-form__input_type_description');
 let addProfileName = document.querySelector('.profile__name-title')
 let addProfileDescription = document.querySelector('.profile__info-description')
+
+
+function copyInfo () {  //заполнение имени и профессии из уже введенных
+  inputProfileName.value = addProfileName.textContent;  //перенесли текст из уже введенного на страницы в поле ввода  
+  inputProfileNameDescription.value = addProfileDescription.textContent; //то же  
+  console.log ('вызвали copyinfo')
+}
 
 let editFormOpen = function () {
   editForm.classList.add('edit-form_open'); 
   pagecover.classList.add('page__cover_open') //делает прозрачным слой над страницей
+  copyInfo ();
 };
 
 let editFormGlose = function () {
@@ -24,12 +32,10 @@ let saveFormName = document.querySelector('.form');
 
 function addName(evt) {
   evt.preventDefault();
-  addProfileName.innerHTML = `
-  ${inputProfileName.value}
-  `
-  addProfileDescription.innerHTML = `
-  ${inputProfileNameDescription.value}
-  `
+  addProfileName.textContent = inputProfileName.value;
+  addProfileDescription.textContent = inputProfileNameDescription.value;
+  editFormGlose();
 }
 
 saveFormName.addEventListener('submit', addName) 
+
