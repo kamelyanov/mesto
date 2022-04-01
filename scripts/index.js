6b198067d54300ce82c5934b19b8b0e097f329e4
Roman Kamelianov <r.kamelyanov@yandex.ru>
2022-04-01 04:16:40 +0300

    fixed rename card to Card

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 import {FormValidator} from './FormValidator.js';
-import {Card} from './Cards.js';
+import {Card} from './Card.js';
 import {initialCards} from './initialCards.js';
 
 const validationSettings = {
